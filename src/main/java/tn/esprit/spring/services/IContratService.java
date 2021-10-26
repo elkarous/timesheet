6889334eb4bea08a20eabc23fb1c60a2e46ec5b4
OumaimaAyadi17 <oumaima.ayadi1@esprit.tn>
2021-10-26 21:34:40 +0100

    log4j +JUnit test
    log4j +JUnit test

--- src/main/java/tn/esprit/spring/services/IContratService.java
+++ src/main/java/tn/esprit/spring/services/IContratService.java
@@ -7,9 +7,12 @@ import tn.esprit.spring.entities.Contrat;
 
 public interface IContratService {
 	
-	
 	public List<Contrat> getAllContrats();
-
+	public Integer ajouterContrat(Contrat contrat);
+	public Contrat affecterContratAEmploye(int contratId, int employeId);
+	public int deleteContratById(int contratId);
+	public void deleteAllContratJPQL();
+	
 
 	
 	
