02e86625b621172e77fc89c55eb167d3cf92f553
bougharra-mohamed <82418258+bougharra-mohamed@users.noreply.github.com>
2021-11-16 15:48:17 +0100

    test 3

--- src/main/java/tn/esprit/spring/dto/ContratDto.java
+++ src/main/java/tn/esprit/spring/dto/ContratDto.java
@@ -7,13 +7,37 @@ import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import tn.esprit.spring.entities.Employe;
+
 public class ContratDto {
+
+	public ContratDto(Date dateDebut2, String string, int i) {
+		// TODO Auto-generated constructor stub
+	}
 
 	@Temporal(TemporalType.DATE)
 	private Date dateDebut;
 	
+
+	
+	private String typeContrat;
+	
+
+	private float telephone;
+
+	
 	@OneToOne
-	private EmployeDto employe;
+	private Employe employe;
+
+	public ContratDto(Date dateDebut, String typeContrat, float salaire) {
+		super();
+		this.dateDebut = dateDebut;
+		this.typeContrat = typeContrat;
+		this.salaire = salaire;
+	}
+
+	private float salaire;
+	
 
 	
 }
