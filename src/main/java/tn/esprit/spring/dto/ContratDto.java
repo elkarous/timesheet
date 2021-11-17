3063d2e212512a598af2dc5b7f2cc6317ffd6ddc
Shekib-yah <57287314+Shekib-yah@users.noreply.github.com>
2021-11-17 16:39:21 +0100

    correction

--- src/main/java/tn/esprit/spring/dto/ContratDto.java
+++ src/main/java/tn/esprit/spring/dto/ContratDto.java
@@ -7,37 +7,13 @@ import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import tn.esprit.spring.entities.Employe;
-
 public class ContratDto {
-
-	public ContratDto(Date dateDebut2, String string, int i) {
-		// TODO Auto-generated constructor stub
-	}
 
 	@Temporal(TemporalType.DATE)
 	private Date dateDebut;
 	
-
-	
-	private String typeContrat;
-	
-
-	private float telephone;
-
-	
 	@OneToOne
-	private Employe employe;
-
-	public ContratDto(Date dateDebut, String typeContrat, float salaire) {
-		super();
-		this.dateDebut = dateDebut;
-		this.typeContrat = typeContrat;
-		this.salaire = salaire;
-	}
-
-	private float salaire;
-	
+	private EmployeDto employe;
 
 	
 }
--- src/test/java/tn/esprit/spring/ContratServiceImplTests.java
+++ src/test/java/tn/esprit/spring/ContratServiceImplTests.java
@@ -1,7 +1,5 @@
 package tn.esprit.spring;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.CoreMatchers.any;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.mockito.BDDMockito.given;
@@ -20,16 +18,13 @@ import org.junit.runner.RunWith;
 import org.mockito.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.datasource.init.ScriptException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.spring.entities.Contrat;
-import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.repository.ContratRepository;
 import tn.esprit.spring.repository.EmployeRepository;
 import tn.esprit.spring.services.ContratServiceImpl;
 
-import tn.esprit.spring.services.IContratService;
 
 
 @RunWith(SpringRunner.class)
